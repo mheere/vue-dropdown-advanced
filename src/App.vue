2c87791661a761d80279c7826521cbe2bb28412d
mheere <marcelheeremans@gmail.com>
2019-04-22 20:50:32 +0100

    first test vue cli

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <Banner position='bottom' hello='marcel'>This is a banner - testing by marcel...</Banner>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Banner from "./components/Banner.vue";
+
+export default Vue.extend({
+  name: "app",
+  components: {
+    Banner
+  }
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

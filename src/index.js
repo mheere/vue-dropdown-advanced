267a6f631b41609113ad6e87624e1f22e2d4be27
mheere <marcelheeremans@gmail.com>
2019-04-24 18:08:29 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-// import Vue from "vue";
-
-//import './styles/flex.css';
-
-//import './utils'
 
 import Banner from "./components/Banner.vue";
 
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-// import { fas } from '@fortawesome/free-solid-svg-icons'
-// import { far } from '@fortawesome/free-regular-svg-icons'
-
-// FontAwesome - Add all icons to the library so you can use it in your page
-// library.add(fas, far)
-
-// Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-// Vue.config.productionTip = false;
-
-// -----------------------------------------
-// specific exports for npm package
-// re-export so it comes from a single package
-// --------------------------------------------------
-//export { Banner } from "./components/Banner.vue";
 export { Banner };
+
+// export { DropDownMenu, DropDownControl } from './components/DropDownMenu';
+// export { DropDownItemBase, HeaderItem, DropDownItem, ActionItem, OptionItem, SeperatorItem, DropDownDirection } from './components/DropDownItems';
+// export { TestData } from './data';
---
+++ src/init.ts
@@ -0,0 +1,335 @@
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+// import { DropDownItemBase, HeaderItem, DropDownItem, ActionItem, OptionItem, SeperatorItem, DropDownDirection } from './components/DropDownItems';
+// import { DropDownMenu, DropDownControl } from './components/DropDownMenu';
+// import { TestData } from './data';
+// import * as __utils from './utils';
+
+// import './styles/rdropdown.scss';
+// import './styles/demo.scss';
+// require('font-awesome/css/font-awesome.css');
+
+// // 
+// // -----------------------------
+
+// var createOnTheFly = (el: any, e: any) => {
+//     //debugger;
+//     console.log("createOnTheFly");
+
+//     var dd = new DropDownControl(el);
+
+//     dd.setToRelativePositionIfNotSet = true;    
+//     dd.openOnCreate = true;
+    
+//     // 2. specify the dropdown items
+//     dd.getItems = () => TestData.getItems("simple items");
+//     dd.direction = DropDownDirection.DownRight;
+    
+//     // 3. 
+//     dd.closeOnActionItemClick = true;
+    
+//     // 4. place event handlers
+//     dd.onClick = (item) => {
+//         console.log(`Item '${item.key}' was clicked. [key: ${item.key}]`);
+//     }
+//     dd.onChecked = (item, checkedOptionItems, allCheckedOptionItems) => {
+//         console.log(`'${item.text}' was clicked [checked: ${item.isChecked}]`);
+//     }
+//     dd.onClose = (item) => {
+//         var txt = item ? item.text : " no item was clicked";
+//         console.log("popup closed - last item: " + txt);
+//     };
+    
+//     // 4. create the react dropdown
+//     dd.createMenu();
+// }
+
+// // 1. on-the-fly - when clicked I want to create the dropdown and AUTO-OPEN - then destroy when closed!
+// var elFly = document.getElementById('ex-create-on-fly');
+// elFly.onclick = (e) => createOnTheFly(elFly, e);
+
+// var elFly2 = document.getElementById('ex-create-on-fly2');
+// elFly2.onclick = (e) => createOnTheFly(elFly2, e);
+
+// // -----------------------------
+
+// // 1. create a new DropDownControl for this element
+// var dd = new DropDownControl('#ex1');
+// //var dd = new DropDownControl('.ex-right-img');
+// //var dd = new DropDownControl('#abc001');
+// dd.setToRelativePositionIfNotSet = true;    
+
+// // 2. specify the dropdown items
+// dd.getItems = () => TestData.getItems("simple items");
+// dd.direction = DropDownDirection.DownRight;
+
+// // 3. 
+// dd.closeOnActionItemClick = true;
+
+// // 4. place event handlers
+// dd.onClick = (item) => {
+//     console.log(`Item '${item.key}' was clicked. [key: ${item.key}]`);
+//     //if (item.key == "cancel") dd.
+// }
+// dd.onChecked = (item, checkedOptionItems, allCheckedOptionItems) => {
+//     console.log(`'${item.text}' was clicked [checked: ${item.isChecked}]`);
+// }
+// dd.onClose = (item) => {
+//     var txt = item ? item.text : " no item was clicked";
+//     console.log("popup closed - last item: " + txt);
+// };
+
+//  // 4. create the react dropdown
+//  dd.createMenu();
+
+//  // -----------------------------
+
+//  // 1. create a new DropDownControl for this element
+//  var el2 = document.getElementById('ex2');
+// var dd2 = new DropDownControl(el2);
+
+// // 2. specify the dropdown items
+// dd2.getItems = () => TestData.getItems("dynamic items");
+
+// // 3. 
+// dd2.closeOnActionItemClick = true;
+
+// // 4. place event handlers
+// dd2.onClick = (item) => {
+//     console.log(`Item '${item.key}' was clicked. [key: ${item.key}]`);
+//     //if (item.key == "cancel") dd.
+// }
+// dd2.onChecked = (item, checkedOptionItems, allCheckedOptionItems) => {
+//     console.log(`'${item.text}' was clicked [checked: ${item.isChecked}]`);
+// }
+// dd2.onClose = (item) => {
+//     var txt = item ? item.text : " no item was clicked";
+//     console.log("popup closed - last item: " + txt);
+// };
+
+//  // 4. create the react dropdown
+// dd2.createMenu();
+
+
+// // -----------------------------
+
+//  // 1. create a new DropDownControl for this element
+//  var el2 = document.getElementById('ex-dyn-async');
+// var dd2 = new DropDownControl(el2);
+
+// // 2. specify the dropdown items
+// dd2.getItemsAsync = () => {
+//     return new Promise<DropDownItemBase[]>((resolve, reject) => {
+//         setTimeout( () => resolve(TestData.getItems("dynamic items")), 1000);
+//     });
+// }
+
+// // 3. 
+// dd2.closeOnActionItemClick = true;
+
+// // 4. place event handlers
+// dd2.onClick = (item) => {
+//     console.log(`Item '${item.key}' was clicked. [key: ${item.key}]`);
+//     //if (item.key == "cancel") dd.
+// }
+// dd2.onChecked = (item, checkedOptionItems, allCheckedOptionItems) => {
+//     console.log(`'${item.text}' was clicked [checked: ${item.isChecked}]`);
+// }
+// dd2.onClose = (item) => {
+//     var txt = item ? item.text : " no item was clicked";
+//     console.log("popup closed - last item: " + txt);
+// };
+
+//  // 4. create the react dropdown
+// dd2.createMenu();
+
+
+
+// // 1. create a new DropDownControl for this element
+// //var dd = new DropDownControl('#ex1');
+// var dd = new DropDownControl('#ex-right-img');
+// //var dd = new DropDownControl('#abc001');
+// dd.setToRelativePositionIfNotSet = true;
+
+// // 2. specify the dropdown items
+// dd.getItems = () => TestData.getItems("action-img-items");
+// dd.direction = DropDownDirection.DownRight;
+
+// // 3. 
+// dd.closeOnActionItemClick = true;
+
+// // 4. place event handlers
+// dd.onClick = (item) => {
+//     var aitem = item as ActionItem;
+//     console.log(`Item '${item.key}' was clicked. [key: ${item.key}] - clickedImage: ${aitem.clickedImage}`);
+//     //if (item.key == "cancel") dd.
+// }
+// dd.onChecked = (item, checkedOptionItems, allCheckedOptionItems) => {
+//     console.log(`'${item.text}' was clicked [checked: ${item.isChecked}]`);
+// }
+// dd.onClose = (item) => {
+//     var txt = item ? item.text : " no item was clicked";
+//     console.log("popup closed - last item: " + txt);
+// };
+
+//  // 4. create the react dropdown
+//  dd.createMenu();
+
+// // -----------------------------
+
+//  // 1. create a new DropDownControl for this element
+//  var dd3 = new DropDownControl('#ex3');
+
+//  // 2. specify the dropdown items
+//  dd3.getItems = () => TestData.getItems("radiooption");
+ 
+//  // 3. 
+//  dd3.closeOnActionItemClick = true;
+ 
+//  // 4. place event handlers
+//  dd3.onClick = (item) => {
+//      console.log(`Item '${item.key}' was clicked. [key: ${item.key}]`);
+//      //if (item.key == "cancel") dd.
+//  }
+//  dd3.onChecked = (item, checkedOptionItems, allCheckedOptionItems) => {
+//      console.log(`'${item.text}' was clicked [checked: ${item.isChecked}]`);
+//  }
+//  dd3.onClose = (item) => {
+//      var txt = item ? item.text : " no item was clicked";
+//      console.log("popup closed - last item: " + txt);
+//  };
+ 
+//   // 4. create the react dropdown
+//  dd3.createMenu();
+
+
+//  // ------------------------------------------
+
+//  // 1. create a new DropDownControl for this element
+//  var dd4 = new DropDownControl('#ex4');
+
+//  // 2. specify the dropdown items
+//  dd4.getItems = () => TestData.getItems("showcase");
+ 
+//  // 3. 
+//  dd4.closeOnActionItemClick = true;
+ 
+//  // 4. place event handlers
+//  dd4.onClick = (item) => {
+//      console.log(`Item '${item.key}' was clicked. [key: ${item.key}]`);
+//      //if (item.key == "cancel") dd.
+//  }
+//  dd4.onChecked = (item, checkedOptionItems, allCheckedOptionItems) => {
+//      console.log(`'${item.text}' was clicked [checked: ${item.isChecked}]`);
+//  }
+//  dd4.onClose = (item) => {
+//      var txt = item ? item.text : " no item was clicked";
+//      console.log("popup closed - last item: " + txt);
+//  };
+ 
+//   // 4. create the react dropdown
+//  dd4.createMenu();
+
+
+// // props this component exposes
+// interface DropDownDemo1Props { clickHandler: (item: DropDownItem) => void }
+
+// // 
+// class DropDownDemo1 extends React.Component<DropDownDemo1Props, {}> {
+
+//     constructor(props: DropDownDemo1Props) {
+//         super(props);
+
+//         // this.fixedItems.push(new ActionItem("logout", "Logout", "fa-window-close-o"));
+//         // this.fixedItems.push(new SeperatorItem());
+//         // this.fixedItems.push(new ActionItem("profile", "Show Profile", "face"));        // fa-user-o
+//         // this.fixedItems.push(new ActionItem("shortcuts", "Show Shortcuts", "report_problem"));  // fa-mail-forward
+
+//         // var item = new ActionItem("setting", "System Settings", "fa-cog");
+//         // item.addRightImage("fa-ellipsis-h", "popup config screen");
+//         // item.textMarginRight = 15;
+//         // this.fixedItems.push(item);
+
+
+//         //ActionItem.useMaterialImage24 = true;
+
+//         var item = new ActionItem("logout", "Logout", "exit_to_app");
+//         //item.className = "md-24";
+//         this.fixedItems.push(item); 
+//         this.fixedItems.push(new SeperatorItem());
+//         this.fixedItems.push(new ActionItem("profile", "Show Profile", "face")); 
+//         this.fixedItems.push(new ActionItem("shortcuts", "Show Shortcuts"));
+
+//         var item = new ActionItem("setting", "System Settings", "report_problem");
+//         item.addRightImage("assignment_ind", "popup config screen");
+//         item.textMarginRight = 15;
+//         this.fixedItems.push(item);
+
+//     }
+
+//     private fixedItems: DropDownItemBase[] = [];
+
+//     private onClick = (item: DropDownItem) => {
+//         // show a console log
+//         console.log(`Item '${item.text}' was clicked. [key: ${item.key}]`);
+
+//         // pass it back up to the caller.
+//         this.props.clickHandler(item);
+//     }
+
+//     private onHover = (item: DropDownItem) => {
+
+//         if (item)
+//             console.log(`Hovering over item: '${item.text}' [key: ${item.key}]`);
+//         else
+//             console.log("No hover");
+
+//     }
+
+//     private getItemsSync = () => {
+//         var arr: DropDownItemBase[] = [];
+//         arr.push(new ActionItem("SYNC", "SYNC", "fa-window-close-o"));
+//         arr.push(new ActionItem("logout", "Logout", "fa-window-close-o"));
+//         arr.push(new SeperatorItem());
+//         arr.push(new ActionItem("profile", "Show Profile", "fa-user-o"));
+//         arr.push(new ActionItem("shortcuts", "Show Shortcuts", "fa-mail-forward"));
+//         arr.push(new ActionItem("setting", "System Settings", "fa-cog"));
+//         return arr;            
+//     }
+
+//     private getItemsAsync = () => {
+//         return new Promise<DropDownItemBase[]>((resolve, reject) => {
+//             setTimeout( () => {
+//                 var arr: DropDownItemBase[] = [];
+//                 arr.push(new ActionItem("logout", "Logout", "fa-window-close-o"));
+//                 arr.push(new SeperatorItem());
+//                 arr.push(new ActionItem("profile", "Show Profile", "fa-user-o"));
+//                 arr.push(new ActionItem("shortcuts", "Show Shortcuts", "fa-mail-forward"));
+//                 arr.push(new ActionItem("setting", "System Settings", "fa-cog"));
+            
+//                 resolve(arr)
+//             } , 500);
+//         });
+//     }
+
+    
+
+//     render() {
+//         return <div id='root2'>
+//                     user: marcel
+//                     <DropDownMenu 
+//                         getItemsAsync={this.getItemsAsync}
+//                         onClick={this.onClick} 
+//                         onHover={this.onHover}
+//                         direction={DropDownDirection.DownLeft} />
+//                 </div>
+//     }
+// }
+
+// // example of using the click outside
+// var onClick = (item: DropDownItem) => { console.log("caller click: " + item.key)};
+
+// // render
+// ReactDOM.render(<DropDownDemo1 clickHandler={onClick}  />, document.getElementById('test001'));
+
+

f17a3dd58f0ecd027e8fa74f63fffbd5ab622075
mheere <marcelheeremans@gmail.com>
2019-05-08 18:36:07 +0100

    updates

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 import DropDownMenu, { DropDownInfo } from "./DropDownMenu.vue"
-import { DropDownItemBase, ActionItem, HeaderItem, DropDownDirection } from "./DropDownItems.ts";
+import { DropDownItemBase, ActionItem, HeaderItem, DropDownDirection, SeperatorItem, CheckboxItem, RadioboxItem, RightImageInfo } from "./DropDownItems.ts";
 import { getTestItems } from '../data'
 import { delay, createGuidRight5 } from '../utils'
 
-export default { DropDownMenu, DropDownInfo, DropDownItemBase, ActionItem, 
+export default { DropDownMenu, DropDownInfo, DropDownItemBase, ActionItem, SeperatorItem, CheckboxItem, RadioboxItem, RightImageInfo, 
 		HeaderItem, DropDownDirection, getTestItems, delay, createGuidRight5 }
 

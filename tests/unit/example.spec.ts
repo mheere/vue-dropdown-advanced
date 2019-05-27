187eb5d0be0a3cbe7cd00753adc27244f670b346
marcel heeremans <marcelheeremans@gmail.com>
2019-05-27 17:36:23 +0100

    updates

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,42 +1,70 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import DropDownMenu from '@/components/DropDownMenu.vue'
-//import Banner from '@/components/Banner.vue'
-import { DropDownItemBase } from '@/components/DropDownItems'
+import { DropDownItemBase, ActionItem, SeperatorItem } from '@/components/DropDownItems'
 import { getTestItems } from '@/data';
-
-// describe('Banner.vue', () => {
-//   it('renders props.msg when passed', () => {
-//     const wrapper = shallowMount(Banner, {
-//       propsData: { position: 'bottom' }
-//     })
-//     expect(wrapper.isVueInstance()).toBeTruthy();
-//   })
-// })
+import Vue from 'vue';
 
 describe('DropDownMenu.vue', () => {
-  var newDiv = document.createElement("div"); 
+  
+  var divParent = document.createElement("div"); 
+  const items : DropDownItemBase[] = [];  // = getTestItems("simple");    // showcase
+  items.push(new ActionItem("A", "Holiday in France", "", true));
+  items.push(new SeperatorItem());
+  items.push(new ActionItem("B", "Go to California "));
+  items.push(new ActionItem("C", "Visit the United Kingdom"));
+
+  const wrapper = shallowMount(DropDownMenu, {
+    propsData: { 
+      parent: divParent,
+      items: items,
+      direction: 'down-right'
+    }
+  })
+
   it('DropDownMenu exists...', () => {
-    const wrapper = shallowMount(DropDownMenu, {
-      propsData: { 
-        parent: newDiv
-      }
-    })
     expect(wrapper.isVueInstance()).toBeTruthy();
   })
-})
 
-describe('DropDownMenu.vue', () => {
-  var newDiv = document.createElement("div"); 
-  const items : DropDownItemBase[] = getTestItems("logout-simple");
   it('It has constructed the hidden dropdown `dda-container` placeholder', () => {
-    const wrapper = shallowMount(DropDownMenu, {
-      propsData: { 
-        parent: newDiv, 
-        items: items,
-        direction: 'down-right' 
-      }
-    })
     expect(wrapper.contains('.dda-container')).toBe(true)
     expect(wrapper.contains('.dda-dropdown-list')).toBe(true)
   })
+
+  it('button click (with simple context) should show 3 dropdown items', () => {
+    divParent.click();
+    expect(wrapper.findAll('.dda-dropdown-item').length).toBe(3)
+  })
+
+  it('whilst the dropdown is visible lets add another item and make sure it is there', () => {
+    items.push(new ActionItem("TEST", "added this test item"));
+    expect(wrapper.findAll('.dda-dropdown-item').length).toBe(4)
+  })
+
+  it('button click should collapse the dropdown', () => {
+    divParent.click();
+    expect(wrapper.findAll('.dda-dropdown-item').length).toBe(0)
+  })
+
+  it('dropdown item click should collapse the dropdown', () => {
+    divParent.click();  // to open the dropdown (to populate it)
+    expect(wrapper.findAll('.dda-dropdown-item').length).toBe(4)
+    //wrapper.find('.dda-dropdown-item').trigger('click');
+    //expect(wrapper.findAll('.dda-dropdown-item').length).toBe(0)
+    
+    // Vue.nextTick(() => {
+    //   expect(wrapper.findAll('.dda-dropdown-item').length).toBe(0)
+    // });
+    
+
+  })
+
+  // wrapper.find('button').trigger('click')
+  //expect(wrapper.find("span").isVisible()).toBe(true)
+  //expect(wrapper.find(Child).exists()).toBe(false)
+  //expect(wrapper.find({ name: "Child" }).exists()).toBe(true)
+  //expect(wrapper.findAll(Child).length).toBe(3)
+
+
+
 })
+
